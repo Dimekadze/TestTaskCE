2d7d8cbc6cb99a84b0265113da5c561f1b610b43
Dimekadze <newakk74@mail.ru>
2026-05-05 13:04:27 +0500

    v10-final

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -9,8 +9,8 @@ export function header_render() {
   return `
     <header class="header">
       <nav class="header__navbar">
-        <img src="/src/assets/logo2.svg" alt="Navigation">
-        <img src="/src/assets/ic_menu_48px.svg" alt="Navigation">
+        <img src="/assets/logo2.svg" alt="Navigation">
+        <img src="/assets/ic_menu_48px.svg" alt="Navigation">
       </nav>
       <div class="header__top">
         <div class="header__top--blocks">
@@ -25,7 +25,7 @@ export function header_render() {
             </div>
             <div class="header--button">${button1.render()}</div>
           </div>
-          <img src="/src/assets/illustration.svg" alt="Illustration">
+          <img src="/assets/illustration.svg" alt="Illustration">
         </div>
       </div>
     </header>
